88ca73ba0319598737ff778c7ca22e2fbdb82bf5
Rumesha Disanayaka <2019cs017@stu.ucsc.lk>
2023-01-15 00:13:48 +0530

    Update home and navbar

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,25 @@
+import {useState} from "react";
+
 //arrow functions
 const Home = () => {
+
+    const [name,setName] = useState('mario');
+    const [age,setAge] = useState('25');
+
+    const handleClick = () => {
+        setName('luvi');
+        setAge(30);
+    }
+    const handleClickAgain = (name) => {
+        console.log("Hello" + name);
+    }
+
     return (
         <div className="home">
             <h2>Home</h2>
+            <p>{ name } is {age} years old</p>
+            <button onClick={handleClick}>Click me</button>
+
         </div>
     );
 }
