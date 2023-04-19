acafe25a8496b3893dc64ffc40159c4332dc621d
Rumesha Disanayaka <2019cs017@stu.ucsc.lk>
2023-04-19 15:28:59 +0530

    Add file updates

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,8 +1,7 @@
-const BlogList = ({blogs,title}) => {
+const BlogList = ({blogs,title,handleDelete}) => {
 
     // const blogs = props.blogs;
     // const title = props.title;
-
     return (
         <div className="blog-list">
             <h2>{title}</h2>
@@ -10,6 +9,7 @@ const BlogList = ({blogs,title}) => {
                 <div className="blog-preview" key={blog.id}>
                     <h2>{blog.title}</h2>
                     <h2>Written by {blog.author}</h2>
+                    {/*<button onClick={() => handleDelete(blog.id)}>Delete Blog</button>*/}
                 </div>
             )) }
         </div>
--- src/Home.js
+++ src/Home.js
@@ -1,17 +1,20 @@
-import {useState} from "react";
+import {useState,useEffect} from "react";
 import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 
 //arrow functions
 const Home = () => {
 
     // const [name,setName] = useState('mario');
     // const [age,setAge] = useState('25');
-    const [blogs,setBlogs] = useState([
-        {title:'A',body:'B',author:'C',id:1},
-        {title:'A',body:'B',author:'C',id:2},
-        {title:'A',body:'B',author:'C',id:3},
-    ])
-
+    // const [blogs,setBlogs] = useState([
+    //     {title:'A',body:'B',author:'C',id:1},
+    //     {title:'A',body:'B',author:'C',id:2},
+    //     {title:'A',body:'B',author:'C',id:3},
+    // ])
+    // const [blogs,setBlogs] = useState(null)
+    // const [isPending,setIsPending] = useState(true)
+    // const [error,setError]=useState(null);
     // const handleClick = () => {
     //     setName('luvi');
     //     setAge(30);
@@ -20,12 +23,31 @@ const Home = () => {
     //     console.log("Hello" + name);
     // }
 
+    // const [name,setName]=useState('naro')
+
+    // const handleDelete = (id) => {
+    //     const newBlogs = blogs.filter(blog => blog.id!==id)
+    //     setBlogs(newBlogs);
+    // }
+
+    // useEffect(()=> {
+    //     console.log('use effect');
+    //     console.log(name);
+    // },[name]);
+
+    const {data:blogs,isPending,error} = useFetch('http://localhost:8000/blogs')
+
     return (
         <div className="home">
             {/*<h2>Home</h2>*/}
             {/*<p>{ name } is {age} years old</p>*/}
             {/*<button onClick={handleClick}>Click me</button>*/}
-            <BlogList blogs={blogs} title="All Blogs!"/>
+            {/*{blogs && <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete}/>}*/}
+            { error && <div>{error}</div> }
+            { isPending && <div>loading....</div> }
+            {blogs && <BlogList blogs={blogs} title="All Blogs!"/>}
+            {/*<button onClick={()=>setName('lugi')}>Change</button>*/}
+            {/*<p>{name}</p>*/}
         </div>
     );
 }
